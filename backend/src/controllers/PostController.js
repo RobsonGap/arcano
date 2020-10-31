0cbee6b063dff27cb2ae54477f177b7ef71c40ff
Robsongap <robsonmie@hotmail.com>
2020-10-31 14:36:58 -0300

    Update PostController.js
    Implementando a lógica para salvar os dados do upload no banco de dados da aplicação ele irá gera um ID para possiveis consultas no banco de dados.

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -6,9 +6,18 @@ module.exports = {
   },
 
   async store(req, res) {
-    //console.log(req.body);
-    //console.log(req.file);
+    const { author, place, description, hashtags } = req.body;
+    const { filename: image } = req.file;
+    //console.log(req.body);//console.log(req.file);
+    const post = await Post.create({
+      author,
+      place,
+      description,
+      hashtags,
+      image,
+    })
 
-    return res.json({ ok: true });
+
+    return res.json({post});
   }
 };
