7efd76bbd4e40ef95ff96f89752b4b86d0a096b2
Robsongap <robsonmie@hotmail.com>
2020-10-31 14:15:35 -0300

    Implementando lógica no upload.js close#15
    Ao fazer o upload de imagens o arquivo será salvo no storage com o nome original do arquivo

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -6,8 +6,8 @@ module.exports = {
   },
 
   async store(req, res) {
-    console.log(req.body);
-    console.log(req.file);
+    //console.log(req.body);
+    //console.log(req.file);
 
     return res.json({ ok: true });
   }
