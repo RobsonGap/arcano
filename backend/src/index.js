d986b8b897a7a6c90316d6ac0caff2e25cebc13b
Robsongap <robsonmie@hotmail.com>
2020-11-01 17:28:08 -0300

    Permitindo que o back-end retorne eventos
    Ao gerar novos posts,likes,etc.. será gerado pelo emit do socket.io passando para todos os usuários o que foi gerado ou criado foi instalado a lib web.socket.io nossa aplicação trabalha com http e reconhece outros tipos de servidor.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,18 @@ const cors = require('cors');
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect('mongodb+srv://semana:semana@cluster0.temy2.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true, useUnifiedTopology: true,
 });
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  next();
+})
 
 app.use(cors());
 
@@ -15,4 +24,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resiz
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
