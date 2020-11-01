d43aeabc7cd6be80d35fab789e3c804baf0401f8
Robsongap <robsonmie@hotmail.com>
2020-11-01 17:10:13 -0300

    Instalando o CORS.js
    Instalando a lib cors e permitindo acesso a nossa aplicação por qualquer front end mesmo estando em servidores diferentes.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://semana:semana@cluster0.temy2.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true, useUnifiedTopology: true,
 });
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
