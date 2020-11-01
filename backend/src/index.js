f1e385bc394af28e9ff5baac79c1d48d1cda975f
Robsongap <robsonmie@hotmail.com>
2020-11-01 16:59:57 -0300

    Gerando link para ás imagens
    Salvando ás imagens e gerando link para poder acessar salvando imagens como jpeg e não como png usando a função do express e usando o path do node.js.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://semana:semana@cluster0.temy2.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true, useUnifiedTopology: true,
 });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
